e2b8423b3574bdc945f35a07662e24f0f44e10e4
BSJ02 <zasiak0323@gmail.com>
2024-04-16 23:15:55 +0900

    BSJ

---
+++ Assets/03.Scripts/Tile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Coord coord;
+
+    // 타일 변수 설정
+    public void SetCoord(int x, int y, bool isWall)
+    {
+        coord = new Coord(x, y, isWall);
+    }
+}
+ 
+[System.Serializable]
+public class Coord
+{
+    public int x;
+    public int y;
+
+    public Coord(int _x, int _y, bool _isWall)
+    {
+        this.x = _x;
+        this.y = _y;
+        this.isWall = _isWall;
+    }
+
+    public Tile parentNode;
+
+    public bool isWall;
+
+    public int G, H;
+
+    public int F { get { return G + H; } }
+
+
+}
