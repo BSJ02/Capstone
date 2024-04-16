0f37919accaa495b88cd2cc71d2b1ac4bfc1bfd7
1bio <leeckdgns07@naver.com>
2024-04-16 22:46:50 +0900

    LCH

---
+++ Assets/02.Scritps/SkillData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkillData", menuName = "ScriptableObject/SkillData", order = 3)]
+public class SkillData : ScriptableObject
+{
+    /*[Header("스킬1")]
+
+    [Header("스킬2")]
+
+    [Header("스킬3")]*/
+
+}
