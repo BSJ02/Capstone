038d90afa5fae27a94559b0f76ae51b4f47e9881
ESYUL1KE <mytom5925@gmail.com>
2024-04-30 14:43:57 +0900

    CSH

--- Assets/03.CSH/03.Scripts/GameManager.cs
+++ Assets/03.CSH/03.Scripts/GameManager.cs
@@ -9,6 +9,7 @@ using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
+    public PlayerData playerData;
     public Player player;
 
     public Text remainingDistance;
@@ -26,21 +27,6 @@ public class GameManager : MonoBehaviour
 
     //게임 턴 판별 private 으로 바꿔주기 
     public bool isGameOver = false; // 게임 오버 상태
-    private GameObject gameOverUI;
-
-    public bool gameturn = true; // true 플레이어 턴 / false 적 턴
-    public GameObject playerturn;
-    public GameObject enemyturn;
-
-
-    //턴 페이드 인 / 아웃
-    private CanvasGroup cg;
-    public float fadeTime = 1f;
-    float accumTime = 0f;
-    private Coroutine fadeCor;
-
-    //bool 플레이어 턴
-
 
 
 
@@ -80,21 +66,12 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         //remainingDistance.text = player.playerData.activePoint.ToString();
-        if (isGameOver)
+        if(playerData.Hp == 0 && !isGameOver)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            isGameOver = true;
         }
 
     }
-
-    public void OnGameOver()
-    {
-        isGameOver = true;
-        gameOverUI.SetActive(true);
-    }
-
-    
-
 
     public void ShowLayerWindow(int index)
     {
@@ -113,72 +90,6 @@ public class GameManager : MonoBehaviour
         windowUI[index].gameObject.SetActive(false);
     }
 
-    public void ShowTurn()
-    {
-        if (gameturn == true)
-        {
-            playerturn.SetActive(true);
-        }
-        else
-        {
-            enemyturn.SetActive(true);
-        }
-        
-    }
-
-   
-
-    public void StartFadeIn()
-    {
-
-        if (gameturn == true)
-        {
-            cg = playerturn.GetComponent<CanvasGroup>();
-        }
-        else
-        {
-            cg = enemyturn.GetComponent<CanvasGroup>();
-        }
-
-        if (fadeCor != null)
-        {
-            StopAllCoroutines();
-            fadeCor = null;
-        }
-        fadeCor = StartCoroutine(FadeIn());
-    }
-
-
-    public IEnumerator FadeIn()
-    {
-        yield return new WaitForSeconds(0.2f);
-        accumTime = 0f;
-        while(accumTime < fadeTime)
-        {
-            cg.alpha = Mathf.Lerp(0f, 1f, accumTime / fadeTime);
-            yield return 0;
-            accumTime += Time.deltaTime;
-        }
-        cg.alpha = 1f;
-
-        StartCoroutine(FadeOut());
-    }
-
-    public IEnumerator FadeOut()
-    {
-        yield return new WaitForSeconds(3.0f);
-        accumTime = 0f;
-        while (accumTime < fadeTime)
-        {
-            cg.alpha = Mathf.Lerp(1f, 0f, accumTime / fadeTime);
-            yield return 0;
-            accumTime += Time.deltaTime;
-        }
-        cg.alpha = 0f;
-
-    }
-
-  
 
     public void cardUIEffect(bool boolean)
     {
@@ -200,12 +111,22 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void RetryGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
-        /* 기능
+    public void ExitGame()
+    {
+        Application.Quit();
+        //SceneManager.LoadScene(MainScene);
+    }
+
+    /* 기능
 
 
-        public void Start, Save ...
-         */
-    
+    public void Start, Save ...
+     */
+
 
 }
